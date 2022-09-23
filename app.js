7e18f90f7abc1a98938868bbb41016de7441c31f
Orel <orel1220@gmail.com>
2022-09-23 13:42:21 +0300

    login and home page added

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'))
 
-require('dotenv').config()
+require('dotenv').config()//אני רוצה שהפרוייקט יכיר את כל הסביבת עבודה שלנו
+app.get('/',function(req,res){ //דף נחיתה- איזה כתובת היוזר הכניס כלומר דף הבית,ת
+    res.sendFile(__dirname+'/index.html')
 
-app.listen(process.env.PORT)
+})
+
+app.get('/login',function(req,res){//דף התחברות
+    res.sendFile(__dirname+'/login.html')
+    
+})
+app.listen(3000,function(){
+    console.log('server is running on port 3000')
+})
