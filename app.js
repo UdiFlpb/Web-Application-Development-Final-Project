23c45a64e1281fce6838c82fe9de0bb30aaf7578
RotemCohen55 <rotemcohen890@gmail.com>
2022-10-13 01:49:25 +0300

    add login fiels

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ app.use(express.static('public'))
 
 require('dotenv').config()
 
+// add db conection
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { 
+    useUnifiedTopology: true, 
+    useNewUrlParser: true 
+});
+
+const session = require('express-session');
+app.use(session({
+    secret: 'foo',    
+    saveUninitialized: false,
+    resave: false
+}))
+
 //this is the home page
 app.get('/items',require('./routes/items'))
 
@@ -16,6 +30,9 @@ app.get('/search',require('./routes/items'))
 
 //this is the type result page
 app.get('/type',require('./routes/items'))
+
+app.use(express.urlencoded({ extended: false }));  
+app.use("/", require("./routes/login"));
 
 app.listen(process.env.PORT,function(){
     console.log('server is running on port: ' + process.env.PORT)
