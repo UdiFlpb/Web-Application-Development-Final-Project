5a260cbfa29d65eee1b2f2b302e8ab3f0acbdbaa
UdiFlpb <udiflpb@gmail.com>
2022-10-13 10:53:21 +0300

    added more photos

--- services/items.js
+++ services/items.js
@@ -1,7 +1,7 @@
 const item = require('../models/items');
 const mongoose = require('mongoose')
 require('dotenv').config()
-mongoose.connect(process.env.MongoConnectionString)
+mongoose.connect(process.env.MONGO_URI)
 
 function getItems() {
     return item.find()
